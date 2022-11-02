a3eee21967119904f260bcc9f2d25651908974ec
thomas <thomas@pop-os.localdomain>
2022-11-03 10:34:18 +1300

    test 10

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
+var Version = "undefined"
+var CommitHash = "undefined"
+
 func main() {
 	fmt.Println("START")
+	fmt.Println(Version, CommitHash)
 	robotgo.TypeStr("Hello World")
 	robotgo.KeyTap("enter")
 	fmt.Println("END")
